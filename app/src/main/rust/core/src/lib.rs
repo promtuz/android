a3e48abd537785cafeb665f6d948cd46da0ea26b
iambhuv <notbhuvnesh@gmail.com>
2025-11-10 15:28:45 +0530

    restored `getStaticKeypair`

--- app/src/main/rust/core/src/lib.rs
+++ app/src/main/rust/core/src/lib.rs
@@ -18,45 +18,31 @@
 //! ```
 
 use jni::JNIEnv;
-use jni::objects::{JClass};
+use jni::objects::{JClass, JObject, JValue};
 use jni::sys::jobject;
 
-// use crate::utils::get_pair_object;
-// pub mod crypto;
-// pub mod insecure;
-// pub mod quic;
+use crate::utils::get_pair_object;
 pub mod utils;
 
+use libcore::get_static_keypair;
 use macros::jni;
 
-/// for eg.
-///
-/// ```kotlin
-/// package com.promtuz.rust
-///
-/// class Core {
-///  external fun getStaticKeypair(): Pair
-/// }
-/// ```
-///
-/// Java_com_promtuz_rust_Core_
 #[jni(base = "com.promtuz.rust", class = "Core")]
 pub extern "C" fn getStaticKeypair(
     mut env: JNIEnv, 
     _class: JClass
 ) -> jobject {
-    todo!("fixing in next commit")
-    // let (secret, public) = get_static_keypair();
+    let (secret, public) = get_static_keypair();
 
-    // let secret_bytes = secret.to_bytes();
-    // let public_bytes = public.to_bytes();
+    let secret_bytes = secret.to_bytes();
+    let public_bytes = public.to_bytes();
 
-    // let secret_jarray = env.byte_array_from_slice(&secret_bytes).unwrap();
-    // let public_jarray = env.byte_array_from_slice(&public_bytes).unwrap();
+    let secret_jarray = env.byte_array_from_slice(&secret_bytes).unwrap();
+    let public_jarray = env.byte_array_from_slice(&public_bytes).unwrap();
 
-    // get_pair_object(
-    //     &mut env,
-    //     JValue::Object(&JObject::from(secret_jarray)),
-    //     JValue::Object(&JObject::from(public_jarray)),
-    // )
+    get_pair_object(
+        &mut env,
+        JValue::Object(&JObject::from(secret_jarray)),
+        JValue::Object(&JObject::from(public_jarray)),
+    )
 }
